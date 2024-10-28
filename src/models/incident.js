acbc5b4d20fc598575865fd12e89fd1253f48316
Ln0Z <113213346+Ln0Z@users.noreply.github.com>
2024-10-29 08:07:07 +1100

    Temporary storage

--- src/models/incident.js
+++ src/models/incident.js
@@ -2,9 +2,20 @@ const db = require('../../config/db.js');
 
 // CREATE
 const createItem = (details, date, time, location, victimId, witnessId, offenderId, urgency, impact, riskAssessment, manager, status, callback) => {
+    console.log('Starting createItem function');
+    console.log('Parameters:', { details, date, time, location, victimId, witnessId, offenderId, urgency, impact, riskAssessment, manager, status });
+    
     const sql = `INSERT INTO incident (details, date, time, location, victimId, witnessId, offenderId, urgency, impact, riskAssessment, manager, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    console.log('SQL Query:', sql);
+    
     db.run(sql, [details, date, time, location, victimId, witnessId, offenderId, urgency, impact, riskAssessment, manager, status], function(err) {
-        callback(err, { id: this.lastID });
+        if (err) {
+            console.error("Error executing insert:", err.message);
+            callback(err, null);
+        } else {
+            console.log("Insert successful, Incident ID:", this.lastID);
+            callback(null, { id: this.lastID });
+        }
     });
 };
 
@@ -16,8 +27,22 @@ const readItem = (callback) => {
 
 // UPDATE
 const updateItem = (id, details, date, time, location, victimId, witnessId, offenderId, urgency, impact, riskAssessment, manager, status, callback) => {
+    console.log('Starting updateItem function');
+    console.log('Updating incident with ID:', id);
+    console.log('New values:', { details, date, time, location, victimId, witnessId, offenderId, urgency, impact, riskAssessment, manager, status });
+
     const sql = `UPDATE incident SET details = ?, date = ?, time = ?, location = ?, victimId = ?, witnessId = ?, offenderId = ?, urgency = ?, impact = ?, riskAssessment = ?, manager = ?, status = ? WHERE id = ?`;
-    db.run(sql, [details, date, time, location, victimId, witnessId, offenderId, urgency, impact, riskAssessment, manager, status, id], callback);
+    console.log('SQL Query:', sql);
+
+    db.run(sql, [details, date, time, location, victimId, witnessId, offenderId, urgency, impact, riskAssessment, manager, status, id], function(err) {
+        if (err) {
+            console.error("Error executing update:", err.message);
+            callback(err);
+        } else {
+            console.log("Update successful for Incident ID:", id);
+            callback(null);
+        }
+    });
 };
 
 // DELETE
